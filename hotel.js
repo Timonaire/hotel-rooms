950249e824fea32a03a843217b6b29d24ce94294
Timonaire <timothyikechukwu94@gmail.com>
2023-02-20 14:39:59 +0100

    hotel update

--- hotel.js
+++ hotel.js
@@ -67,3 +67,57 @@ app.get('/room-types/search', (req, res) => {
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['guest', 'admin'],
+    default: 'guest'
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
+
+const { authenticateUser, authorizeUser, validateData } = require('./authMiddleware');
+
+// Creating many room types
+app.post('/room-types', authenticateUser, authorizeUser('admin'), validateData, (req, res) => {
+  const codeName = req.body.codeName;
+  const prize = req.body.prize;
+
+  let roomType = new RoomType(codeName, prize);
+  roomTypes.push(roomType);
+  res.send(`Room type with code name ${codeName} and prize ${prize} created.`);
+});
+
+// Editing a room type using id
+app.put('/room-types/:id', authenticateUser, authorizeUser('admin'), validateData, (req, res) => {
+  const id = req.params.id;
+  const codeName = req.body.codeName;
+  const prize = req.body.prize;
+
+  let roomType = roomTypes[id];
+  roomType.codeName = codeName;
+  roomType.prize = prize;
+  res.send(`Room type with id ${id} edited.`);
+});
+
+// Deleting room type using id
+app.delete('/room-types/:id', authenticateUser, authorizeUser('admin'), (req, res) => {
+  const id = req.params.id;
+});
